a698e22abaf0372584e972aa2dbaf8aac0f833ab
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2018-06-28 22:37:48 +0200

    Refactor layout styling

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,18 +1,20 @@
 import Head from 'next/head';
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20
-}
-
 const Layout = (props) => (
   <React.Fragment>
     <Head>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
     </Head>
-    <div style={layoutStyle}>
+    <div className="layout">
       {props.children}
     </div>
+    <style jsx>{`
+      .layout {
+        margin: 0 auto;
+        max-width: 1000px;
+        padding: 20px;
+      }
+    `}</style>
   </React.Fragment>
 );
 
