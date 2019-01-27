d00166b097c752c9fbb3a2aa60158726dd2ead60
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2019-01-27 12:59:52 +0100

    Fix bug after renaming proxy flag

--- server.js
+++ server.js
@@ -1,55 +1,58 @@
-const express = require('express');
-const proxy = require('express-http-proxy');
-const next = require('next');
-const mockService = require('./service/mock');
+const express = require('express')
+const proxy = require('express-http-proxy')
+const next = require('next')
+const mockService = require('./service/mock')
 
-const uiRoute = require('./routes/ui');
-const mocksConfigurationRoute = require('./routes/mocksConfiguration');
+const uiRoute = require('./routes/ui')
+const mocksConfigurationRoute = require('./routes/mocksConfiguration')
 
-const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || '3030';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || '3030'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-app.prepare()
+app
+  .prepare()
   .then(mockService.initializeMocks)
   .then(() => {
-    const server = express();
+    const server = express()
 
-    server.use('/ui', uiRoute(app));
+    server.use('/ui', uiRoute(app))
 
-    server.use('/mocks', mocksConfigurationRoute(app));
+    server.use('/mocks', mocksConfigurationRoute(app))
 
     server.get('/_next*', (req, res) => {
       return handle(req, res)
-    });
+    })
 
     server.use('*', async (req, res, next) => {
-      const mocks = await mockService.getAll();
-      const foundMock = mocks.filter(mock => mock.url === req.baseUrl && mock.method === req.method)[0];
-      if (foundMock) {
-        if (foundMock.enabled) {
-          if (foundMock.delay > 0) {
-            await delay(foundMock.delay);
-          }
-          res.status(foundMock.status).json(foundMock.data);
-          return;
-        }
-        if (foundMock.proxyUrl) {
-          return proxy(foundMock.proxyUrl)(req, res, next);
-        }
-      }
-      res.status(404).end();
-    });
+      const mocks = await mockService.getAll()
+      const foundMock = mocks.find(
+        mock => mock.url === req.baseUrl && mock.method === req.method
+      )
 
-    server.listen(port, (err) => {
+      if (foundMock) {
+        if (foundMock.proxyEnabled && foundMock.proxyUrl) {
+          return proxy(foundMock.proxyUrl)(req, res, next)
+        }
+
+        if (foundMock.delay > 0) {
+          await delay(foundMock.delay)
+        }
+        res.status(foundMock.status).json(foundMock.data)
+        return
+      }
+      res.status(404).end()
+    })
+
+    server.listen(port, err => {
       if (err) throw err
       console.log(`> Ready on http://localhost:${port}/ui`)
     })
   })
-  .catch((ex) => {
+  .catch(ex => {
     console.error(ex.stack)
     process.exit(1)
   })
